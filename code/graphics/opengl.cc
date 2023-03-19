d28f2b8dd3221ae745f3abda9dfb262b49af331d
Andy C <xndc@users.noreply.github.com>
2023-03-19 12:05:44 +0000

    Enable OpenGL debug messages

--- code/graphics/opengl.cc
+++ code/graphics/opengl.cc
@@ -2,6 +2,22 @@
 
 #include "base/debug.hh"
 
+#if DEBUG && PLATFORM_DESKTOP
+#define ENABLE_GL_DEBUG_MODE 1
+void GLAPIENTRY GLDebugMessageCallback (GLenum src, GLenum type, GLuint id, GLenum sev, GLsizei len,
+	const char* msg, const void* uparam)
+{
+	switch (type) {
+		case GL_DEBUG_TYPE_ERROR: { LOG_F(INFO, "GL error: %s", msg); break; }
+		case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: { LOG_F(INFO, "GL deprecation warning: %s", msg); break; }
+		case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: { LOG_F(INFO, "GL UB warning: %s", msg); break; }
+		case GL_DEBUG_TYPE_PORTABILITY: { LOG_F(INFO, "GL portability warning: %s", msg); break; }
+		case GL_DEBUG_TYPE_PERFORMANCE: { LOG_F(INFO, "GL performance warning: %s", msg); break; }
+		default: { LOG_F(INFO, "GL message: %s", msg); }
+	}
+}
+#endif
+
 SDL_GLContext GLCreateContext(SDL_Window* window) {
 	#if PLATFORM_DESKTOP || PLATFORM_MOBILE
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
@@ -10,6 +26,9 @@ SDL_GLContext GLCreateContext(SDL_Window* window) {
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 		// FIXME: Remove once we start using FBOs
 		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 32);
+	#endif
+	#if ENABLE_GL_DEBUG_MODE
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
 	#endif
 	SDL_GLContext gl_context = SDL_GL_CreateContext(window);
 	CHECK_NOTNULL_F(gl_context, "Failed to create OpenGL context: %s", SDL_GetError());
@@ -23,5 +42,15 @@ void GLMakeContextCurrent(SDL_Window* window, SDL_GLContext context) {
 	#if PLATFORM_DESKTOP
 		int gl_version = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
 		CHECK_NE_F(gl_version, 0, "Failed to load OpenGL functions");
+	#endif
+
+	#if ENABLE_GL_DEBUG_MODE
+		if (glDebugMessageCallback) {
+			glDebugMessageCallback(GLDebugMessageCallback, nullptr);
+			glEnable(GL_DEBUG_OUTPUT);
+			const char message[] = "OpenGL debug messages enabled";
+			glDebugMessageInsert(GL_DEBUG_SOURCE_APPLICATION, GL_DEBUG_TYPE_OTHER, 0,
+				GL_DEBUG_SEVERITY_NOTIFICATION, sizeof(message), message);
+		}
 	#endif
 }
